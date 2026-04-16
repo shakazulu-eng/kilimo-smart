51fdfab88a131204a8fa7efa54f88a7d1920be07
shakazulu-eng <salummuhidini748@gmail.com>
2026-04-16 12:43:01 -0400

    Add default admin user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +21,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+           URL::forceScheme('https');
         //
+         if (!User::where('email', 'salummuhidini748@gmail.com')->exists()) {
+        User::create([
+            'name' => 'salumu',
+            'email' => 'salummuhidini748@gmail.com',
+            'password' => Hash::make('Mlanz9.8765432100'),
+            'role' => 'admin'
+        ]);
+    }
+
     }
 }
