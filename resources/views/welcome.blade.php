8298d2c1d5d50cd13b1d4509e15a40efe2a2e0da
shakazulu-eng <salummuhidini748@gmail.com>
2026-04-17 08:36:40 -0400

    Updated homepage UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,16 +31,19 @@
                             Dashboard
                         </a>
                     @else
-                           <a href="{{ route('login') }}"
-   class="inline-block px-5 py-2 bg-orange-500 text-white rounded-lg hover:bg-orange-600 transition duration-300">
-   Log in
-</a>
+                          <a
+                            href="{{ route('login') }}"
+                            class="inline-block px-5 py-1.5 text-green-600 text-[#16a34a] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
+                        >
+                            Log in
+                        </a> 
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}"
-   class="inline-block px-5 py-2 bg-orange-500 text-white rounded-lg hover:bg-orange-600 transition duration-300">
-   Register
-</a>
+                            <a
+                                href="{{ route('register') }}"
+                                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                                Register
+                            </a>
                         @endif
                     @endauth
                 </nav>
