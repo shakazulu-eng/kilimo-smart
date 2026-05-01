4314ea350afe06e4796e46d95cb0b1f18659f737
shakazulu-eng <salummuhidini748@gmail.com>
2026-05-01 05:11:29 -0400

    FINAL FIX

--- app/Http/Controllers/AIController.php
+++ app/Http/Controllers/AIController.php
@@ -48,7 +48,7 @@ class AIController extends Controller
             // 🌦️ GET WEATHER
             $weatherResponse = Http::get("https://api.openweathermap.org/data/2.5/weather", [
                 'q' => 'Dar es Salaam',
-                'appid' => env('OPENWEATHER_API_KEY'),
+                'appid' => env('WEATHER_API_KEY'),
                 'units' => 'metric'
             ]);
 
