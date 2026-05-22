7d52e14f74998bae769b06a2a7707db917a87c96
shakazulu-eng <salummuhidini748@gmail.com>
2026-05-22 15:50:36 -0400

    temporary admin route

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -30,7 +30,7 @@
     <button type="submit">Logout</button>
 </form>
 
-/*<a href="{{ route('test.ai') }}"
+<a href="{{ route('test.ai') }}"
    class="fixed bottom-5 right-5 z-50 group flex items-center gap-2">
 
     <!-- Text -->
@@ -70,5 +70,5 @@
 
     </div>
 
-</a>*/
+</a>
 @endsection
